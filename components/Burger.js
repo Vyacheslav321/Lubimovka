1b3d276b739d0221c47f558c138079669d73b69a
Ivan Monichev <ivan@monichev.ru>
2022-06-25 01:01:54 +0300

    feat: add burger menu on 1024 width

---
+++ components/Burger.js
@@ -0,0 +1,16 @@
+export class Burger {
+  constructor(navSelector, burgerSelector) {
+    this.navElement = document.querySelector(navSelector);
+    this.burgerElement = document.querySelector(burgerSelector);
+  }
+
+  _handleToggle() {
+    this.navElement.classList.toggle('nav_is-opened');
+    this.navElement.style = 'transition: margin .6s ease-in-out;'
+    this.burgerElement.classList.toggle('menu-burger_is-active');
+  }
+
+  handleListeners() {
+    this.burgerElement.addEventListener('click', () => this._handleToggle());
+  }
+}

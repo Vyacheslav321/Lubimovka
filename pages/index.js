1f60ab11eb486a21fa9e31c96fac8dd05d754e29
Ivan Monichev <ivan@monichev.ru>
2022-06-25 19:01:48 +0300

    feat: add burger menu on 645 width

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,18 @@
 import {Burger} from "../components/Burger.js";
 
-const burgerNavigation = new Burger('.nav', '.menu-burger');
-burgerNavigation.handleListeners();
+const burgerElement = document.querySelector('.menu-burger');
+const burgerTopNavigation = new Burger('nav', 'menu-burger');
+const burgerScrollNavigation = new Burger('header__body', 'menu-burger');
+
+const activateNavigation = () => {
+  const screenWidth = window.screen.width;
+  if (screenWidth <= 645) {
+    burgerScrollNavigation.handleToggle();
+    burgerScrollNavigation.disableScroll();
+  } else {
+    burgerTopNavigation.handleToggle();
+  }
+}
+
+burgerElement.addEventListener('click', activateNavigation)
+

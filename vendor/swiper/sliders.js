bd29c9f16d857273d335a1642091a5d45760aba3
Ivan Monichev <ivan@monichev.ru>
2022-07-01 23:51:14 +0300

    feat: add slider

--- vendor/swiper/sliders.js
+++ vendor/swiper/sliders.js
@@ -3,19 +3,28 @@ import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
 export const reviewSlider = new Swiper('.review-slider', {
 
   // Optional parameters
-  slidesPerView: 'auto',
-  centeredSlides: true,
+  
   speed: 800,
   spaceBetween: 30,
-  loop: true,
   pagination: {
     el: ".review-slider__pagination",
     clickable: true,
   },
-
+  
   // Navigation arrows
   navigation: {
     nextEl: '.review-slider__button-next',
     prevEl: '.review-slider__button-prev',
   },
+
+  breakpoints: {
+    0: {
+      centeredSlides: false,
+      slidesPerView: 1,
+    },
+    645: {
+      centeredSlides: true,
+      slidesPerView: 'auto',
+    }
+  }
 })
